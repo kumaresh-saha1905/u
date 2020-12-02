0e3b674f93ea74b93d92a2f85f75c34da17e38ea
kumaresh-saha1905 <74669883+kumaresh-saha1905@users.noreply.github.com>
2020-12-02 21:11:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+ var fixedRect, movingRect;
+
+ function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  movingRect.velocityX=1;
+  movingRect.velocityY=1;
+  fixedRect.velocityX=-1;
+  fixedRect.velocityY=1;
+}
+
+function draw() {
+  background(0,0,0);  
+ /* movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;*/
+  bounceoff(movingRect,fixedRect);
+  /*if(isTouching(fixedRect,movingRect)){
+      movingRect.shapeColor="red";
+      fixedRect.shapeColor="red";
+
+  }
+  else{ 
+      movingRect.shapeColor="green";
+      fixedRect.shapeColor="green";
+ 
+  }*/
+
+  drawSprites();
+}
